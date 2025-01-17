003f7d741f78ea087e1a57355a976297d17bb3ec
youssefrabie12 <youssefrabie122@gmail.com>
2025-01-17 20:04:43 +0200

    Update script.js

--- Day #78 - Google Gemini Clone/script.js
+++ Day #78 - Google Gemini Clone/script.js
@@ -9,7 +9,7 @@ const clearChatButton = document.getElementById("deleteButton");
 let currentUserMessage = null;
 let isGeneratingResponse = false;
 
-const GOOGLE_API_KEY = "YOUR_API_KEY";
+const GOOGLE_API_KEY = "AIzaSyAMNugBkicYaZpf_WgScLy0ZBIxGVZoU1g";
 const API_REQUEST_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${GOOGLE_API_KEY}`;
 
 // Load saved data from local storage
